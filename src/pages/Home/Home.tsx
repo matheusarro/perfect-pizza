8d3d7c492cfce12716abf1dc81996b6d4752fbc9
Matheus Araujo <matheus.arro@gmail.com>
2024-07-16 19:47:23 -0300

    feat: add home component

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import Button from '@/components/Button/Button';
+import { ArrowDownIcon } from '@heroicons/react/16/solid';
+
+function Home() {
+  return (
+    <div className="flex h-full w-full flex-row gap-4 bg-red-100">
+      <Button variant="primary" icon={<ArrowDownIcon className="size-6" />}>
+        Botão Primário
+      </Button>
+      <Button variant="secondary">Botão Secundário</Button>
+      <Button variant="tertiary">Botão Terciário</Button>
+    </div>
+  );
+}
+
+export default Home;
