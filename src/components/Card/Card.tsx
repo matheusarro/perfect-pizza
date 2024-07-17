f72685fdf58fd8fbc5d671ef01dbffd0310560b0
Matheus Araujo <matheus.arro@gmail.com>
2024-07-17 10:03:32 -0300

    feat: add selected prop to card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,10 +2,17 @@ import { ReactNode } from 'react';
 
 type CardProps = {
   children: ReactNode;
+  selected?: boolean;
 };
 
-function Card({ children }: CardProps) {
-  return <div className={'flex h-full w-full flex-row gap-4 rounded-3xl bg-white p-8 shadow-lg'}>{children}</div>;
+function Card({ children, selected = false }: CardProps) {
+  return (
+    <div
+      className={`flex h-full w-full flex-row gap-4 rounded-3xl bg-white p-8 shadow-lg ${selected ? 'border-2 border-orange-500 shadow-orange-500' : ''}`}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Card;
