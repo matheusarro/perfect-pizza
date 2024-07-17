b640061b0821623407af22a04c06f88c1efbc098
Matheus Araujo <matheus.arro@gmail.com>
2024-07-16 21:36:44 -0300

    feat: add pizza sizes cards

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,4 @@
+import Button from '@/components/Button/Button';
 import Card from '@/components/Card/Card';
 import Header from '@/components/Header/Header';
 
@@ -8,21 +9,32 @@ function Home() {
 
       <h2>Selecione um tamanho de pizza</h2>
 
-      <Card>
-        <div className="flex flex-col gap-4 md:flex-row">
-          <img
-            src="https://img.freepik.com/fotos-premium/uma-pizza-com-calabresa-e-cortada-em-oito-fatias_1010706-438.jpg"
-            alt="Pequena"
-            width={150}
-            height={150}
-          />
-          <span>Tamanho</span>
-          <span>R$ 20,20</span>
-          <span>Adicionar</span>
-        </div>
-      </Card>
+      {PIZZA_SIZES.map((pizza) => (
+        <Card>
+          <div className="align-center flex w-full flex-col items-center justify-center gap-4 md:flex-row">
+            <img
+              src="https://img.freepik.com/fotos-premium/uma-pizza-com-calabresa-e-cortada-em-oito-fatias_1010706-438.jpg"
+              alt="Pequena"
+              className="w-full md:w-[200px]"
+            />
+            <span className={'text-xl font-bold'}>{pizza.size}</span>
+            <span className={'text-xl font-bold text-orange-500'}>
+              {pizza.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
+            </span>
+            <Button>{'Selecionar'}</Button>
+          </div>
+        </Card>
+      ))}
     </div>
   );
 }
 
 export default Home;
+
+// MOCK
+
+const PIZZA_SIZES = [
+  { size: 'Pequena', price: 20.2 },
+  { size: 'Média', price: 30.3 },
+  { size: 'Grande', price: 40 },
+];
