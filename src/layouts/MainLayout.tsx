c31fcb718983ffc751a6d366d7434776247bae6d
Matheus Araujo <matheus.arro@gmail.com>
2024-07-16 22:06:38 -0300

    fix: height on main layout component

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 
 function MainLayout() {
   return (
-    <div className="pt-16">
+    <div className="h-screen pt-16">
       <NavBar />
-      <div className="flex flex-col items-center">
+      <div className="flex h-full w-full flex-col items-center">
         <div className="container p-4">
           <Outlet />
         </div>
