fb2febf0317fb5294d1d16b01f923823a81bc93d
Matheus Araujo <matheus.arro@gmail.com>
2024-07-17 10:27:21 -0300

    feat: remove shadow from button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,14 +6,16 @@ interface ButtonProps {
   icon?: ReactNode;
   variant?: 'primary' | 'secondary' | 'tertiary';
   fullWidth?: boolean;
+  disabled?: boolean;
 }
 
-const Button = ({ children, onClick, icon, variant = 'primary', fullWidth = false }: ButtonProps) => {
+const Button = ({ children, onClick, icon, variant = 'primary', fullWidth = false, disabled = false }: ButtonProps) => {
   const buttonWidth: string = fullWidth ? 'md:w-full' : 'md:w-fit';
 
   const primaryColorClasses = `bg-orange-500 text-white hover:bg-orange-400`;
   const secondaryColorClasses = `bg-white text-black hover:bg-gray-50`;
   const tertiaryColorClasses = `bg-yellow-500 text-black hover:bg-yellow-400`;
+  const disabledColorClasses = `bg-gray-300 text-white`;
 
   const buttonVariants = {
     primary: primaryColorClasses,
@@ -23,7 +25,8 @@ const Button = ({ children, onClick, icon, variant = 'primary', fullWidth = fals
 
   return (
     <button
-      className={`flex w-full items-center justify-center gap-2 rounded-full px-4 py-2 font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-opacity-50 ${buttonWidth} ${buttonVariants[variant]}`}
+      disabled={disabled}
+      className={`flex w-full items-center justify-center gap-2 rounded-full px-4 py-2 font-semibold focus:outline-none focus:ring-2 focus:ring-opacity-50 disabled:cursor-not-allowed ${buttonWidth} ${disabled ? disabledColorClasses : buttonVariants[variant]}`}
       onClick={onClick}
     >
       {icon && icon}
