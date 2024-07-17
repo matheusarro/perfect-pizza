b9a095b3fa96d5e3e6f9f6d12a41dfbe67addbbd
Matheus Araujo <matheus.arro@gmail.com>
2024-07-17 12:23:01 -0300

    feat: add total to order summary

--- src/pages/Summary/Summary.tsx
+++ src/pages/Summary/Summary.tsx
@@ -14,8 +14,40 @@ function Summary() {
     console.log(order);
   };
 
-  const disabledNextButton =
-    !order.find(({ itemID }) => itemID === ID)?.size || !order.find(({ itemID }) => itemID === ID)?.flavor;
+  const handleNewPizza = () => {
+    alert('Conferir log');
+    console.log(order);
+  };
+
+  const orderTotalPrice = () => {
+    let total = 0;
+
+    order.forEach((pizza) => {
+      total += (pizza.size?.price || 0) + (pizza.flavor?.price || 0);
+      if (pizza.additionals) {
+        pizza.additionals.forEach((additional) => {
+          total += additional.price;
+        });
+      }
+    });
+
+    return total;
+  };
+
+  const orderTotalTime = () => {
+    let total = 0;
+
+    order.forEach((pizza) => {
+      total += (pizza.size?.time || 0) + (pizza.flavor?.time || 0);
+      if (pizza.additionals) {
+        pizza.additionals.forEach((additional) => {
+          total += additional.time;
+        });
+      }
+    });
+
+    return total;
+  };
 
   return (
     <div className={'flex h-full w-full flex-col gap-8 md:gap-12'}>
@@ -29,47 +61,47 @@ function Summary() {
       <div className={'flex flex-col gap-4'}>
         <span className={'text-lg font-bold'}>{'Itens do pedido'}</span>
 
-        <div className={'grid w-full grid-cols-6 gap-2'}>
-          <span className={'text-lg font-semibold italic'}>Item</span>
+        <div className={'grid w-full grid-cols-3 gap-2'}>
           <span className={'text-lg font-semibold italic'}>Tamanho</span>
           <span className={'text-lg font-semibold italic'}>Sabor</span>
           <span className={'text-lg font-semibold italic'}>Personalizações</span>
-          <span className={'text-lg font-semibold italic'}>Valor</span>
-          <span className={'text-lg font-semibold italic'}>Tempo</span>
 
           {order.map((pizza) => (
             <>
-              <span>{pizza.itemID}</span>
               <span>{pizza.size?.name}</span>
               <span>{pizza.flavor?.name}</span>
-              <span>
+              <div className={'flex flex-col'}>
                 {pizza.additionals && pizza.additionals?.length > 0
-                  ? pizza.additionals?.map((additional) => additional.name).join(',')
+                  ? pizza.additionals?.map((additional) => (
+                      <span>
+                        {additional.name}
+                        {additional.price > 0 &&
+                          ` + ${additional.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}`}
+                      </span>
+                    ))
                   : '-'}
-              </span>
-              <span>
-                {(
-                  (pizza.size?.price || 0) +
-                  (pizza.flavor?.price || 0) +
-                  (pizza.additionals?.reduce((acc, curr) => acc + curr.price, 0) || 0)
-                ).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
-              </span>
-              <span>
-                {`${
-                  (pizza.size?.time || 0) +
-                  (pizza.flavor?.time || 0) +
-                  (pizza.additionals?.reduce((acc, curr) => acc + curr.time, 0) || 0)
-                } minutos`}
-              </span>
+              </div>
             </>
           ))}
         </div>
       </div>
 
-      <div className="flex justify-center">
-        <Button disabled={disabledNextButton} onClick={() => handleNextStep()}>
-          {'Finalizar pedido'}
+      <div className={'flex flex-col gap-4'}>
+        <span className={'text-lg font-bold'}>{'Totais do pedido'}</span>
+
+        <div className={'grid w-full grid-cols-2 gap-2'}>
+          <span className={'text-lg font-semibold italic'}>Valor</span>
+          <span className={'text-lg font-semibold italic'}>Tempo</span>
+          <span>{orderTotalPrice().toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>
+          <span>{orderTotalTime()} minutos</span>
+        </div>
+      </div>
+
+      <div className="flex flex-row justify-center gap-4">
+        <Button variant="tertiary" onClick={() => handleNewPizza()}>
+          {'Adicionar pizza'}
         </Button>
+        <Button onClick={() => handleNextStep()}>{'Finalizar pedido'}</Button>
       </div>
     </div>
   );
